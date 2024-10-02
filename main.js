2d1249251a04f2b70efea36dd9c7517a398c47d7
gabriel ferri <107404209+ferrigb@users.noreply.github.com>
2024-10-02 12:06:45 -0300

    java top

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Página carregada!");
+
+    // Adicionando o evento de clique ao botão
+    document.getElementById("nintendo-link").addEventListener("click", function() {
+        window.open("https://www.nintendo.com", "_blank");
+    });
 });
