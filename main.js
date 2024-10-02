d9beb8af9bff73e8ffdd9eb1174468aaec547be6
gabriel ferri <107404209+ferrigb@users.noreply.github.com>
2024-10-02 11:58:52 -0300

    fiz style

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-// script.js
-// Exemplo: Mostrar/ocultar conteúdo ao clicar em um botão
-const botao = document.getElementById('meuBotao');
-const conteudo = document.getElementById('meuConteudo');
-
-botao.addEventListener('click', () => {
-    conteudo.classList.toggle('mostrar');
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Página carregada!");
 });
